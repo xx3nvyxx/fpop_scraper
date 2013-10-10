b81db5f62a3743cbc39685a64d78b684cbfe167f
William Washco <xx3nvyxx@gmail.com>
2013-10-10 15:49:33 -0700

    Forgot how arrays work in php
    derp

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
   $accounts = array();
-  $accounts[] = {"user" => "Photon",
-                 "email" => "xx3nvyxx+photon@gmail.com"
-                 "password" => "xxxxxxxxxxx"};
-  $accounts[] = {"user" => "Overdrive",
-                 "email" => "xx3nvyxx+overdrive@gmail.com"
-                 "password" => "xxxxxxxxxxx"};
-  $accounts[] = {"user" => "Mifi",
-                 "email" => "xx3nvyxx+mifi@gmail.com"
-                 "password" => "xxxxxxxxxxx"};
-  $accounts[] = {"user" => "Phone",
-                 "email" => "xx3nvyxx+phone@gmail.com"
-                 "password" => "xxxxxxxxxxx"};
+  $accounts[] = array("user" => "Photon",
+                      "email" => "xx3nvyxx+photon@gmail.com",
+                      "password" => "xxxxxxxxxxx");
+  $accounts[] = array("user" => "Overdrive",
+                      "email" => "xx3nvyxx+overdrive@gmail.com",
+                      "password" => "xxxxxxxxxxx");
+  $accounts[] = array("user" => "Mifi",
+                      "email" => "xx3nvyxx+mifi@gmail.com",
+                      "password" => "xxxxxxxxxxx");
+  $accounts[] = array("user" => "Phone",
+                      "email" => "xx3nvyxx+phone@gmail.com",
+                      "password" => "xxxxxxxxxxx");
 ?>
