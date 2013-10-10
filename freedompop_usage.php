a00a65430786b2943ef0ecce06e9d0dd1a76e7b3
William Washco <xx3nvyxx@gmail.com>
2013-10-10 15:33:36 -0700

    Modify page to work with multiple accounts

--- freedompop_usage.php
+++ freedompop_usage.php
@@ -1,39 +1,48 @@
 <!DOCTYPE html>
 <html>
-  <?php
-    // Basic variables
-    $user = "my";
-
-    require('./config.php');
-    // Populate the constructor!!!!
-    $details = new FpopDetails("me@mydomain.com", "my_password");
-  ?>
-  
   <head>
     <title>
-      <?php $user ?>'s Freedompop Usage Stats
+      Freedompop Usage Stats
     </title>
   </head>
 
   <body>
-    <h2>Freedompop usage for <?php $user ?></h2>
-    <p>
-      Days Left:
-      <?php
-        $details->days_left;
-      ?>
-    </p>
-    <p>
-      Data Used:
-      <?php
-        $details->data_used;
-      ?>
-    </p>
-    <p>
-      Total Data Available:
-      <?php
-        $details->total_data;
-      ?>
-    </p>
+    <?php
+      // Account info
+      require('./config.php');
+
+      //class
+      require('./FpopDetails.php');
+      
+      
+      foreach ($accounts as $account)
+      {
+        $user = $account["user"];
+        $details = new FpopDetails($account["email"], $account["password"]);
+    ?>
+    <div>
+      <h2>Freedompop usage for <?php $user ?></h2>
+      <p>
+        Days Left:
+        <?php
+          $details->days_left;
+        ?>
+      </p>
+      <p>
+        Data Used:
+        <?php
+          $details->data_used;
+        ?>
+      </p>
+      <p>
+        Total Data Available:
+        <?php
+          $details->total_data;
+        ?>
+      </p>
+    </div>
+    <?php
+      }
+    ?>
   </body>
 </html>
